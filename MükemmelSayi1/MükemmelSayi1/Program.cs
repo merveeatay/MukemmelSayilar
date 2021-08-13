b54d774708ed29bc497f8161db7b75f5fd551ae3
merveeatay <83241309+merveeatay@users.noreply.github.com>
2021-08-13 22:09:58 +0300

    Mükemmel Sayı

---
+++ MükemmelSayi1/MükemmelSayi1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MükemmelSayi1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen bir sayı giriniz:");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            int toplam = 0;
+
+            for (int i = 1; i < sayi; i++)
+            {
+                if (sayi % i == 0)
+                {
+                    toplam = toplam + i;
+                }
+            }
+
+            if (toplam == sayi)
+            {
+                Console.WriteLine(sayi + " " + "Mükemmel sayıdır.");
+            }
+            else
+            {
+                Console.WriteLine(sayi + " " + "Mükemmel sayı değildir.");
+            }
+            Console.ReadLine();
+
+        
+    }
+    }
+}
